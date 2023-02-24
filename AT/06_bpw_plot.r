c938adcba9a753a233c57b3daa83c46e0a73b1a7
Nicolai Berk <nicolai.berk@gmail.com>
2023-02-24 11:57:17 +0100

    plots to en

--- AT/06_bpw_plot.r
+++ AT/06_bpw_plot.r
@@ -6,13 +6,49 @@ library(ggplot2)
 setwd("C:/Users/nicol/Dropbox/Studium/Amsterdam/Studies/Semester 4/Master Thesis/GitHub/SMLSE/AT")
 
 ## load weights
-plt  <- read_csv("smlse/AT_weights.csv") %>%
+pre_dta  <- read_csv("smlse/AT_weights.csv") %>%
   mutate(weight_sq = weight**2) %>%
   ## filter top
   slice_max(weight_sq, n = 30) %>% ## plot
   ## sort by weight
-  arrange(desc(weight)) %>%
-  ggplot(aes(x = reorder(feature, weight), y = weight, fill = weight)) +
+  arrange(desc(weight))
+
+pre_dta$lbls_en <- 
+  c(
+    "honored",
+    "SPÖ",
+    "once",
+    "patients [m]",
+    "years",
+    "just",
+    "population",
+    "Vienna",
+    "high",
+    "freedom",
+    "colleague [f]",
+    "yes",
+    "Pilz (Greens)",
+    "area",
+    "also",
+    "Kern (SPÖ)",
+    "want",
+    "appropriate",
+    "finance minister",
+    "Kurz (ÖVP)",
+    "colleagues [m]",
+    "citizens [f]",
+    "motion",
+    "love",
+    "minister [f]",
+    "humans",
+    "FPÖ",
+    "colleagues [f]",
+    "federal minister [f]",
+    "ÖVP"
+  )
+
+pre_dta %>%
+  ggplot(aes(x = reorder(lbls_en, weight), y = weight, fill = weight)) +
   geom_col() +
   labs(x = "Feature", y = "Weight") +
   theme_minimal() +
@@ -23,7 +59,8 @@ plt  <- read_csv("smlse/AT_weights.csv") %>%
     low = "red",
     mid = "white",
     high = "green",
-    space = "Lab")
+    space = "Lab") +
+    theme(legend.position = "None")
 
 ## save plot
-ggsave(filename = "vis/AT_weights.png", width = 4, height = 10)
+ggsave(filename = "vis/AT_weights.png", width = 3, height = 6)
--- DE/06_bpw_plot.r
+++ DE/06_bpw_plot.r
@@ -6,24 +6,62 @@ library(ggplot2)
 setwd("C:/Users/nicol/Dropbox/Studium/Amsterdam/Studies/Semester 4/Master Thesis/GitHub/SMLSE/DE")
 
 ## load weights
-plt  <- read_csv("smlse/DE_weights.csv") %>%
+pre_dta  <- read_csv("smlse/DE_weights.csv") %>%
   mutate(weight_sq = weight**2) %>%
   ## filter top
   slice_max(weight_sq, n = 30) %>% ## plot
   ## sort by weight
-  arrange(desc(weight)) %>%
-  ggplot(aes(x = reorder(feature, weight), y = weight, fill = weight)) +
+  arrange(desc(weight))
+
+pre_dta$lbls_en <- 
+  c(
+    "AfD",
+    "german",
+    "Germany",
+    "old-parties",
+    "here",
+    "government",
+    "Merkel",
+    "a lot",
+    "thank",
+    "one",
+    "thanks",
+    "voters",
+    "EU",
+    "german ",
+    "colleagues",
+    "ask",
+    "important",
+    "coalition agreement",
+    "warmly",
+    "debates",
+    "the left",
+    "ciizens [f]",
+    "hence",
+    "last",
+    "therefor",
+    "say",
+    "democrats",
+    "find",
+    "need",
+    "colleagues [f]"
+
+    )
+
+pre_dta %>%
+  ggplot(aes(x = reorder(lbls_en, weight), y = weight, fill = weight)) +
   geom_col() +
   labs(x = "Feature", y = "Weight") +
   theme_minimal() +
   coord_flip() +
-  ggtitle("NL") +
+  ggtitle("DE") +
   scale_fill_gradient2(
     midpoint = 0,
     low = "red",
     mid = "white",
     high = "green",
-    space = "Lab")
+    space = "Lab") +
+    theme(legend.position = "None")
 
 ## save plot
-ggsave(filename = "vis/DE_weights.png", width = 4, height = 10)
+ggsave(filename = "vis/DE_weights.png", width = 3, height = 6)
